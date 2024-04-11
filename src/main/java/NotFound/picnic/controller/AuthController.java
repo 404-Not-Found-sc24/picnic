3b27939ddf40aae59cd055725586c79f0fd1819b
eun0901 <jk2310400@ajou.ac.kr>
2024-04-11 22:16:19 +0900

    feat: 이미지 업로드 로직 생성

--- src/main/java/NotFound/picnic/controller/AuthController.java
+++ src/main/java/NotFound/picnic/controller/AuthController.java
@@ -5,6 +5,7 @@ import NotFound.picnic.dto.LoginResponseDto;
 import NotFound.picnic.dto.ReissueTokenDto;
 import NotFound.picnic.dto.SignUpDto;
 import NotFound.picnic.service.AuthService;
+import NotFound.picnic.service.S3Upload;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
@@ -15,6 +16,7 @@ import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 
@@ -23,6 +25,7 @@ import java.io.IOException;
 @RequestMapping("/api/auth")
 public class AuthController {
     private final AuthService authService;
+    private final S3Upload s3Upload;
 
 
     @PostMapping("/sign-in")
@@ -40,8 +43,16 @@ public class AuthController {
 
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/test")
-    public String test() {
-        return "success";
+    public String test(@RequestPart(required = false) MultipartFile file) {
+        if (file.isEmpty()) {
+            return "파일이 유효하지 않습니다.";
+        }
+        try {
+            s3Upload.uploadFiles(file, "static");
+            return "파일 업로드 성공";
+        } catch (Exception e) {
+            return "파일 업로드 실패" + e.getMessage();
+        }
     }
 
     @PatchMapping("/reissue-token")
