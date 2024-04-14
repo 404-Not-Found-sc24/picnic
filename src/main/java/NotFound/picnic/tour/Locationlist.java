f5995d27d1198b2aaff55d43f67c1e75e652ade3
apollo211 <suhwan211@ajou.ac.kr>
2024-04-14 22:58:33 +0900

    feat: search_location_by_name_and_address

--- src/main/java/NotFound/picnic/tour/Locationlist.java
+++
@@ -1,9 +0,0 @@
-package NotFound.picnic.tour;
-
-import NotFound.picnic.repository.LocationRepository;
-import NotFound.picnic.domain.Location;
-import org.springframework.web.bind.annotation.*;
-
-public class Locationlist {
-
-}
