720fa6bcaa93a9b0380bdc52f79935172f325f65
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-04-19 01:27:55 +0900

    feat: 장소 보기 api 구현

--- src/main/java/NotFound/picnic/controller/TourController.java
+++ src/main/java/NotFound/picnic/controller/TourController.java
@@ -1,11 +1,26 @@
 package NotFound.picnic.controller;
 
+import NotFound.picnic.dto.LocationGetDto;
+import NotFound.picnic.service.TourService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/tour")
 public class TourController {
+    private final TourService tourService;
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/locations")
+    public ResponseEntity<List<LocationGetDto>> getLocations(@RequestParam(name="city") String city, @RequestParam(required = false, defaultValue = "", name="keyword") String keyword) throws UnsupportedEncodingException {
+        List<LocationGetDto> locationGetDtoList = tourService.GetLocations(city, keyword);
+        return ResponseEntity.ok().body(locationGetDtoList);
+    }
+
 }
--- src/main/java/NotFound/picnic/repository/LocationRepository.java
+++ src/main/java/NotFound/picnic/repository/LocationRepository.java
@@ -2,6 +2,15 @@ package NotFound.picnic.repository;
 
 import NotFound.picnic.domain.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+import java.util.Optional;
 
 public interface LocationRepository  extends JpaRepository<Location, Long> {
+    Optional<List<Location>> findAllByCity(String city);
+
+    @Query("select l from Location l where l.city = :city and (l.name like %:keyword% or l.address like %:keyword%)")
+    Optional<List<Location>> findByCityAndKeyword(@Param("city") String city, @Param("keyword") String keyword);
 }
