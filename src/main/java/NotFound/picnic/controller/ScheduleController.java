5e39c6f95ca748681b9714d11350e476c26dd37e
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-04-18 20:09:18 +0900

    fix: API endpoint 수정

--- src/main/java/NotFound/picnic/controller/ScheduleController.java
+++ src/main/java/NotFound/picnic/controller/ScheduleController.java
@@ -16,7 +16,7 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/schedule")
+@RequestMapping("/schedule")
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
