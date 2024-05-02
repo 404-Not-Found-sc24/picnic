daa458012c2523552df185caf04e00e013c4f575
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-05-02 20:48:48 +0900

    feat: 장소 검색 api에서 도시 필수 아니도록 로직 변경

--- src/main/java/NotFound/picnic/controller/TourController.java
+++ src/main/java/NotFound/picnic/controller/TourController.java
@@ -21,7 +21,7 @@ public class TourController {
     private final TourService tourService;
 
     @GetMapping("/locations")
-    public ResponseEntity<List<LocationGetDto>> getLocations(@RequestParam(name="city") String city, @RequestParam(required = false, defaultValue = "", name="keyword") String keyword, @RequestParam(required = false, defaultValue = "0", name="lastIdx") int lastIdx) throws UnsupportedEncodingException {
+    public ResponseEntity<List<LocationGetDto>> getLocations(@RequestParam(required = false, defaultValue = "", name="city") String city, @RequestParam(required = false, defaultValue = "", name="keyword") String keyword, @RequestParam(required = false, defaultValue = "0", name="lastIdx") int lastIdx) throws UnsupportedEncodingException {
         List<LocationGetDto> locationGetDtoList = tourService.GetLocations(city, keyword, lastIdx);
         return ResponseEntity.ok().body(locationGetDtoList);
     }
