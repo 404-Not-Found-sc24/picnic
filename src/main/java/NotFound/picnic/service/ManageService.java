991c46c44dc7f4cc3492d87bfa9e2976b25159c0
yjoon2k <syjoon2k@gmail.com>
2024-05-09 18:29:41 +0900

    feat: approval approve api

--- src/main/java/NotFound/picnic/service/ManageService.java
+++ src/main/java/NotFound/picnic/service/ManageService.java
@@ -18,6 +18,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.rmi.server.LogStream.log;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -74,20 +76,19 @@ public class ManageService {
         if(approval.getState() != State.APPLIED){
             return "승인할 수 없는 장소입니다.";
         }
-
         approval.setAddress(approveDto.getAddress());
-        approval.setContent(approveDto.getContent());
-        approval.setDetail(approveDto.getDetail());
         approval.setName(approveDto.getName());
+        approval.setDetail(approveDto.getDetail());
+        approval.setContent(approveDto.getContent());
         approval.setState(State.APPROVED);
 
         approvalRepository.save(approval);
 
-        String city = approveDto.getAddress().split(" ")[0];
+        String city = approval.getAddress().split(" ")[0];
         String[] strArray = {"서울특별시", "인천광역시", "대구광역시", "대전광역시", "부산광역시", "울산광역시", "세종특별자치시", "제주특별자치도"};
         List<String> strList = new ArrayList<>(Arrays.asList(strArray));
         if(!strList.contains(city)) {
-            city = city + " " +approveDto.getAddress().split(" ")[1];
+            city = city + " " +approval.getAddress().split(" ")[1];
         }
         Location location = Location.builder()
                 .name(approval.getName())
