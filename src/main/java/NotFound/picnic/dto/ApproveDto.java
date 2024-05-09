7a2428ae111e96b4769adbaea589ef2a13792e25
yjoon2k <syjoon2k@gmail.com>
2024-05-09 18:29:41 +0900

    feat: approval approve api

--- src/main/java/NotFound/picnic/dto/ApproveDto.java
+++ src/main/java/NotFound/picnic/dto/ApproveDto.java
@@ -2,9 +2,11 @@ package NotFound.picnic.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
+@Getter
 public class ApproveDto {
     private String address;
     private String content;
