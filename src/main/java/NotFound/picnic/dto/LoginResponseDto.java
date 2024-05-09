5d5a5a188fd6d9f4c443b98f694140824a215b14
yjoon2k <syjoon2k@gmail.com>
2024-05-09 14:20:45 +0900

    fix: login response modified

--- src/main/java/NotFound/picnic/dto/LoginResponseDto.java
+++ src/main/java/NotFound/picnic/dto/LoginResponseDto.java
@@ -8,4 +8,8 @@ public class LoginResponseDto {
     private String grantType;
     private String accessToken;
     private String refreshToken;
+    private String email;
+    private String name;
+    private String nickname;
+    private String phone;
 }
