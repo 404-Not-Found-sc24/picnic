31f45bd3b5a03ca6a6324e7b2fd809126a65d94f
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-05-16 02:23:07 +0900

    feat: 사용자 정보 수정 api 구현

--- src/main/java/NotFound/picnic/repository/MemberRepository.java
+++ src/main/java/NotFound/picnic/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findMemberByEmail(String email);
+
+    Boolean existsMemberByNickname(String nickname);
 }
