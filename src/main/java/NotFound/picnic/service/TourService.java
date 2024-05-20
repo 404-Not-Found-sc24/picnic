7e7d3948bba56eef52ae7d6fa82ea18443d50cdc
yjoon2k <syjoon2k@gmail.com>
2024-05-20 16:06:00 +0900

    fix: Get Diary Detail longitude latitude add

--- src/main/java/NotFound/picnic/service/TourService.java
+++ src/main/java/NotFound/picnic/service/TourService.java
@@ -300,6 +300,7 @@ public class TourService {
     public DiaryDetailDto GetDiaryDetail(Long diaryId){
         Diary diary = diaryRepository.findById(diaryId).orElseThrow();
         List<Image> images = imageRepository.findAllByDiary(diary);
+        Place place = placeRepository.findByDiary_DiaryId(diaryId);
 
         return DiaryDetailDto.builder()
                 .userName(diary.getPlace().getSchedule().getMember().getName())
@@ -307,6 +308,8 @@ public class TourService {
                 .date(diary.getPlace().getDate())
                 .weather(diary.getWeather())
                 .content(diary.getContent())
+                .longitude(place.getLocation().getLongitude())
+                .latitude(place.getLocation().getLatitude())
                 .imageUrl(images.stream().map(Image::getImageUrl).collect(Collectors.toList()))
                 .build();
     }
