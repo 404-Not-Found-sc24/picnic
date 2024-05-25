500caa0c12a5dea1fa7ba4cc67fff0f85cbdbe34
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-05-26 01:53:26 +0900

    feat: 사용자 삭제 api 구현 (task 54)

--- src/main/java/NotFound/picnic/service/ManageService.java
+++ src/main/java/NotFound/picnic/service/ManageService.java
@@ -363,7 +363,7 @@ public class ManageService {
    }
 
    public String UpdateUser(UserUpdateDto userUpdateDto, Long memberId) {
-        Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+       Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
 
         if (userUpdateDto.getName() != null) member.setName(userUpdateDto.getName());
         if (userUpdateDto.getNickname() != null) member.setNickname(userUpdateDto.getNickname());
@@ -378,5 +378,13 @@ public class ManageService {
         memberRepository.save(member);
 
         return "수정 완료";
+   }
+
+   public String DeleteUser(Long memberId) {
+       Member member = memberRepository.findById(memberId).orElseThrow(() -> new CustomException(ErrorCode.USER_NOT_FOUND));
+
+       memberRepository.delete(member);
+
+       return "삭제 완료";
    }
 }
