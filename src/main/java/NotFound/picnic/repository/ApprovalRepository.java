137950924445d866a312a866002c70c3e2188fa0
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-03 17:31:35 +0900

    feat: 장소 승인 검색 로직 추가

--- src/main/java/NotFound/picnic/repository/ApprovalRepository.java
+++ src/main/java/NotFound/picnic/repository/ApprovalRepository.java
@@ -1,9 +1,12 @@
 package NotFound.picnic.repository;
 
 import NotFound.picnic.domain.Approval;
+import NotFound.picnic.enums.State;
 import lombok.NonNull;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -13,4 +16,6 @@ public interface ApprovalRepository extends JpaRepository<Approval, Long> {
     @NonNull
     List <Approval> findAll();
 
+    @Query(value="select * from approval where (name like concat('%', :keyword, '%') or address like concat('%', :keyword, '%')) and division like concat('%', :division, '%') and state like concat('%',:state,'%') order by name, approval_id", nativeQuery = true)
+    List<Approval> findApprovals(@Param("keyword") String keyword, @Param("division") String division, @Param("state") String state);
 }
