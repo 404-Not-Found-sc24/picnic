bdd981ebd75ebd2edb9b1376a932bcaf18ce4338
Eunjeong Lee <jk2310400@ajou.ac.kr>
2024-06-06 21:35:36 +0900

    fix: Jwt 에러 반환 오류 수정

--- src/main/java/NotFound/picnic/auth/JwtAuthFilter.java
+++ src/main/java/NotFound/picnic/auth/JwtAuthFilter.java
@@ -1,5 +1,7 @@
 package NotFound.picnic.auth;
 
+import NotFound.picnic.exception.CustomException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,10 +10,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
 
+@Component
 @RequiredArgsConstructor
 public class JwtAuthFilter extends OncePerRequestFilter { // 한 번 실행 보장
     private final CustomUserDetailsService customUserDetailsService;
@@ -27,7 +34,9 @@ public class JwtAuthFilter extends OncePerRequestFilter { // 한 번 실행 보�
             String token = authorizationHeader.substring(7);
 
             // JWT 유효성 검증
-            if (jwtUtil.validateToken(token)) {
+            try {
+                // JWT 유효성 검증
+                jwtUtil.validateToken(token);
                 Long userId = jwtUtil.getUserId(token);
 
                 // 유저와 토큰 일치 시 userDetails 생성
@@ -40,10 +49,31 @@ public class JwtAuthFilter extends OncePerRequestFilter { // 한 번 실행 보�
                     // 현재 Request의 Security Context에 접근권한 설정
                     SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
                 }
+            } catch (Exception e) {
+                // 토큰이 유효하지 않거나 만료된 경우
+                if (e instanceof CustomException) {// 정의한 error에 속할 경우
+                    CustomException customException = (CustomException) e;
+                    handleAuthenticationException(response, customException.getMessage());
+                }else{
+                    handleAuthenticationException(response, "internal server error"); // 전체 internal로 처리
+                }
             }
-
         }
 
         filterChain.doFilter(request, response);
+    }
+
+    private void handleAuthenticationException(HttpServletResponse response,String message ) throws IOException {
+        response.setStatus(401);
+        response.setContentType("application/json;charset=UTF-8");
+
+        Map<String, Object> responseBody = new HashMap<>();
+        responseBody.put("status", "FAILED");
+        responseBody.put("message", message);
+
+        PrintWriter writer = response.getWriter();
+        writer.write(new ObjectMapper().writeValueAsString(responseBody));
+        writer.flush();
+        writer.close();
     }
 }
